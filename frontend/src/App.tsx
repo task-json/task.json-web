e0e66519839fa054c383c4331d8ee8c746a6b839
DCsunset <dcsunset@protonmail.com>
2020-12-09 17:49:11 +0800

    feat(frontend): add task filters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,8 @@ import { RootState } from "./store";
 import { Container, FormControl, Grid, InputLabel, makeStyles, MenuItem, Select, TextField, Typography } from '@material-ui/core';
 import { ChevronRight } from '@material-ui/icons';
 import { Autocomplete } from "@material-ui/lab";
-import { getProjects, parseTasks } from './utils/tasks';
+import { getContexts, getProjects, parseTasks } from './utils/tasks';
+import _ from "lodash";
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -26,13 +27,70 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+type View = "Tasks" | "Projects" | "Contexts";
+
 function App() {
   const tasks = useSelector((state: RootState) => parseTasks(state.tasks));
   const classes = useStyles();
-  const [view, setView] = useState("Tasks");
+  const [view, setView] = useState("Tasks" as View);
   const [taskDialog, setTaskDialog] = useState(false);
 
-  const projects = getProjects(tasks);
+  const [projects, setProjects] = useState([] as string[]);
+  const [contexts, setContexts] = useState([] as string[]);
+  const allProjects = getProjects(tasks);
+  const allContexts = getContexts(tasks);
+
+  const filteredTasks = tasks.filter(task => {
+    if (view === "Tasks") {
+      return true;
+    }
+    else if (view === "Projects") {
+      return _.intersection(
+        task.projects,
+        projects
+      ).length === projects.length;
+    }
+    else {
+      return _.intersection(
+        task.contexts,
+        contexts
+      ).length === contexts.length;
+    }
+  });
+
+  const TaskFilter = () => {
+    if (view === "Projects") {
+      return (
+        <Autocomplete
+          multiple
+          className={classes.select}
+          value={projects}
+					onChange={(_, value) => setProjects(value)}
+          options={[...allProjects]}
+          renderInput={params => (
+            <TextField {...params} label="Projects" />
+          )}
+        />
+      );
+    }
+    else if (view === "Contexts") {
+      return (
+        <Autocomplete
+          multiple
+          className={classes.select}
+          options={[...allContexts]}
+          value={contexts}
+					onChange={(_, value) => setContexts(value)}
+          renderInput={params => (
+            <TextField {...params} label="Contexts" />
+          )}
+        />
+      );
+    }
+    else {
+      return <Fragment />;
+    }
+  }
 
   return (
     <Layout>
@@ -48,16 +106,10 @@ function App() {
               <Typography variant="h5">
                 {view}
               </Typography>
-              {view === "Projects" && (
+              {view !== "Tasks" && (
                 <Fragment>
                   <ChevronRight className={classes.chevron} />
-                  <Autocomplete
-                    className={classes.select}
-                    options={[...projects]}
-                    renderInput={params => (
-                      <TextField {...params} label="Project" />
-                    )}
-                  />
+                  <TaskFilter />
                 </Fragment>
               )}
             </Grid>
@@ -68,17 +120,18 @@ function App() {
               <Select
                 labelId="view-select-label"
                 value={view}
-                onChange={event => setView(event.target.value as string)}
+                onChange={event => setView(event.target.value as View)}
               >
                 <MenuItem value="Tasks">Tasks</MenuItem>
                 <MenuItem value="Projects">Projects</MenuItem>
+                <MenuItem value="Contexts">Contexts</MenuItem>
               </Select>
             </FormControl>
           </Grid>
         </Grid>
 
         <TodoList
-          data={tasks}
+          data={filteredTasks}
           onAdd={() => setTaskDialog(true)}
         />
       </Container>
