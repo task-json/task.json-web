2fe35ac0af9a1e72438609138d2f84fa9899f768
DCsunset <dcsunset@protonmail.com>
2020-12-13 15:12:55 +0800

    fix(frontend): remove redundant task filters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,96 +1,31 @@
-import React, { Fragment, useState } from 'react';
+import { useState } from 'react';
 import TodoList from "./components/TodoList";
 import Layout from "./components/Layout";
 import TaskDialog from "./components/TaskDialog";
 import { useSelector } from 'react-redux';
 import { RootState } from "./store";
-import { Container, FormControl, Grid, InputLabel, makeStyles, MenuItem, Select, TextField, Typography } from '@material-ui/core';
-import { ChevronRight } from '@material-ui/icons';
-import { Autocomplete } from "@material-ui/lab";
-import { getContexts, getProjects, parseTasks } from './utils/tasks';
-import _ from "lodash";
+import {
+  Container,
+  makeStyles,
+  Typography
+} from '@material-ui/core';
+import { parseTasks } from './utils/tasks';
 
 const useStyles = makeStyles(theme => ({
-  title: {
-    flexGrow: 1
-  },
   head: {
-    paddingTop: theme.spacing(3)
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3)
   },
   select: {
     minWidth: 120,
     marginBottom: theme.spacing(1.5)
   },
-  chevron: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1)
-  }
 }));
-
-type View = "Tasks" | "Projects" | "Contexts";
 
 function App() {
   const tasks = useSelector((state: RootState) => parseTasks(state.tasks));
   const classes = useStyles();
-  const [view, setView] = useState("Tasks" as View);
   const [taskDialog, setTaskDialog] = useState(false);
-
-  const [projects, setProjects] = useState([] as string[]);
-  const [contexts, setContexts] = useState([] as string[]);
-  const allProjects = getProjects(tasks);
-  const allContexts = getContexts(tasks);
-
-  const filteredTasks = tasks.filter(task => {
-    if (view === "Tasks") {
-      return true;
-    }
-    else if (view === "Projects") {
-      return _.intersection(
-        task.projects,
-        projects
-      ).length === projects.length;
-    }
-    else {
-      return _.intersection(
-        task.contexts,
-        contexts
-      ).length === contexts.length;
-    }
-  });
-
-  const TaskFilter = () => {
-    if (view === "Projects") {
-      return (
-        <Autocomplete
-          multiple
-          className={classes.select}
-          value={projects}
-					onChange={(_, value) => setProjects(value)}
-          options={[...allProjects]}
-          renderInput={params => (
-            <TextField {...params} label="Projects" />
-          )}
-        />
-      );
-    }
-    else if (view === "Contexts") {
-      return (
-        <Autocomplete
-          multiple
-          className={classes.select}
-          options={[...allContexts]}
-          value={contexts}
-					onChange={(_, value) => setContexts(value)}
-          renderInput={params => (
-            <TextField {...params} label="Contexts" />
-          )}
-        />
-      );
-    }
-    else {
-      return <Fragment />;
-    }
-  }
 
   return (
     <Layout>
@@ -100,38 +35,12 @@ function App() {
       />
 
       <Container>
-        <Grid container alignItems="center" className={classes.head}>
-          <Grid item className={classes.title}>
-            <Grid container alignItems="center">
-              <Typography variant="h5">
-                {view}
-              </Typography>
-              {view !== "Tasks" && (
-                <Fragment>
-                  <ChevronRight className={classes.chevron} />
-                  <TaskFilter />
-                </Fragment>
-              )}
-            </Grid>
-          </Grid>
-          <Grid item>
-            <FormControl className={classes.select}>
-              <InputLabel id="view-select-label">View</InputLabel>
-              <Select
-                labelId="view-select-label"
-                value={view}
-                onChange={event => setView(event.target.value as View)}
-              >
-                <MenuItem value="Tasks">Tasks</MenuItem>
-                <MenuItem value="Projects">Projects</MenuItem>
-                <MenuItem value="Contexts">Contexts</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-        </Grid>
+        <Typography className={classes.head} variant="h5">
+          Tasks
+        </Typography>
 
         <TodoList
-          data={filteredTasks}
+          data={tasks}
           onAdd={() => setTaskDialog(true)}
         />
       </Container>
