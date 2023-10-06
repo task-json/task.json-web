3e3c77c5d84759e3a400bddaa9683ba01961655b
DCsunset <DCsunset@protonmail.com>
2023-10-05 23:40:10 -0400

    docs: add README, LICENSE and copyright notice

--- src/components/TaskDialog.tsx
+++ src/components/TaskDialog.tsx
@@ -1,3 +1,18 @@
+// Copyright (C) 2023  DCsunset
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { computedState } from "../store/state";
 import { Autocomplete, Box, Button, Dialog, DialogActions, DialogTitle, FormControl, InputLabel, MenuItem, Select, SxProps, TextField } from "@mui/material";
 import { DateTimePicker } from "@mui/x-date-pickers";
